41fb2b7e50e551e3bde51973314b0317eb40893f
gokulnc@ymail.com <gokulnc@ymail.com>
2022-07-03 11:23:03 +0000

    Support to enable PostgreSQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -112,12 +112,24 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv('POSTGRES_DB_NAME'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.getenv('POSTGRES_DB_NAME'),
+            'USER': os.getenv('POSTGRES_DB_USERNAME'),
+            'PASSWORD': os.getenv('POSTGRES_DB_PASSWORD'),
+            'HOST': os.getenv('POSTGRES_DB_HOST'),
+            'PORT': os.getenv('POSTGRES_DB_PORT'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
