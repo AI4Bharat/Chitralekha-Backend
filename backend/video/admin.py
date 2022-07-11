0af17b17655f559fb98036fdd2d9384fe1e8577b
Nishant Nayak <nishantnayak2001@gmail.com>
2022-07-11 16:03:59 +0530

    Add endpoint to fetch recently used videos
    Signed-off-by: Nishant Nayak <nishantnayak2001@gmail.com>

--- backend/video/admin.py
+++ backend/video/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Video
 
-admin.site.register(Video)
+# Show particular fields in the admin panel
+class VideoAdmin(admin.ModelAdmin):
+    '''
+    VideoAdmin class to render the video admin panel.
+    '''
+    list_display = ('name', 'url', 'duration')
+
+admin.site.register(Video, VideoAdmin)
