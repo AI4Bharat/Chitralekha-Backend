3798eb60af23fdc632c1d5a50e8cdbd57f6803c2
Prakhar Rathi <prakharrathi25@gmail.com>
2022-08-16 09:18:06 +0530

    allow email and username login

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,15 +1,13 @@
-from django.contrib.auth import login
 from django.contrib.auth.models import User
 from knox.models import AuthToken
-from knox.views import LoginView as KnoxLoginView
-from rest_framework import generics, permissions, viewsets
+from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import RegisterSerializer, UserSerializer, LoginUserSerializer
+from .serializers import (LoginUserSerializer, RegisterSerializer,
+                          UserSerializer)
 
 
 # Class based view to Get User Details using Token Authentication
