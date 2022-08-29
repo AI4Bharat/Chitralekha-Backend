401d83ee2ed1273c1787f4b69eddb38a74d5b782
Prakhar Rathi <prakharrathi25@gmail.com>
2022-08-29 16:05:11 +0530

    Remove router from urls where unused

--- backend/video/urls.py
+++ backend/video/urls.py
@@ -3,12 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from . import views
 
-router = DefaultRouter()
-
-router.register(r"", views.VideoViewSet)
-
 urlpatterns = [
     path("", views.get_video, name="get_video"),
     path("list_recent", views.list_recent, name="list_recent"),
-    path("api", include(router.urls)),
 ]
