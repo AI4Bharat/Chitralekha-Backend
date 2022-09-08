b3dcbd2730cb7d8c3d39a74509a70305e433923b
Prakhar Rathi <prakharrathi25@gmail.com>
2022-09-08 18:50:45 +0530

    Add audio_only field to video model

--- backend/video/models.py
+++ backend/video/models.py
@@ -10,6 +10,11 @@ class Video(models.Model):
     url = models.URLField(unique=True, verbose_name="Video URL", db_index=True)
     duration = models.DurationField(verbose_name="Video Duration")
     subtitles = models.JSONField(verbose_name="Subtitles", null=True, blank=True)
+    audio_only = models.BooleanField(
+        verbose_name="Audio Only",
+        default=False,
+        help_text="Does this object only contain audio?",
+    )
 
     def __str__(self):
         return self.name
