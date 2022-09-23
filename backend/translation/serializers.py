05c473d5a0e9d3b030dbccb0af5c8377bb145dd7
Prakhar Rathi <prakharrathi25@gmail.com>
2022-09-23 15:30:42 +0530

    Update serializers.py

--- backend/translation/serializers.py
+++ backend/translation/serializers.py
@@ -4,10 +4,6 @@ from .models import Translation
 
 
 class TranslationSerializer(serializers.ModelSerializer):
-    translation_type = serializers.SerializerMethodField()
-
-    def get_translation_type(self, obj):
-        return obj.get_translation_type_display()
 
     class Meta:
         model = Translation
