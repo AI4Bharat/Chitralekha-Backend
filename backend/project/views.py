b927473ac7a46955b67f800a624b9717df54cac3
ummegith <ummaimaec.rymec@gmail.com>
2022-10-18 15:04:20 +0530

    CL-65-66-67 adding

--- backend/project/views.py
+++ backend/project/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework import status
-
+from users.models import User
 from .models import Project
 from .serializers import ProjectSerializer
 
@@ -16,3 +16,232 @@ class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     permission_classes = (IsAuthenticated,)
+
+    @action(
+        detail=True,
+        methods=["POST"],
+        name="Add Project members",
+        url_name="add_project_members",
+    )
+    def add_project_members(self, request, pk=None, *args, **kwargs):
+        try:
+            project = Project.objects.get(pk=pk)
+            if "user_id" in dict(request.data):
+                ids = request.data.get("user_id", "")
+            else:
+                return Response(
+                    {"message": "key doesnot match"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            user = User.objects.filter(id__in=ids)
+            if not user:
+                return Response(
+                    {"message": "User doesnot exist"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            for prj_user in user:
+                if prj_user in project.members.all():
+                    return Response(
+                        {"error": "member already added"},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+                project.members.add(prj_user)
+                project.save()
+            return Response(
+                {"message": "Project members added successfully"},
+                status=status.HTTP_200_OK,
+            )
+        except Project.DoesNotExist:
+            return Response(
+                {"error": "Project does not exist"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        return Response(
+            {"error": "Method is not allowed"},
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+        )
+
+    # add an endpoint to remove project members
+    @action(
+        detail=True,
+        methods=["POST"],
+        name="Remove Project members",
+        url_name="remove_project_members",
+    )
+    def remove_project_members(self, request, pk=None, *args, **kwargs):
+        try:
+            project = Project.objects.get(pk=pk)
+            if "user_id" in dict(request.data):
+                ids = request.data.get("user_id", "")
+            else:
+                return Response(
+                    {"message": "key doesnot match"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            user = User.objects.filter(id__in=ids)
+            if not user:
+                return Response(
+                    {"message": "User doesnot exist"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            for prj_user in user:
+                if prj_user not in project.members.all():
+                    return Response(
+                        {"error": "member not added"},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+                project.members.remove(prj_user)
+                project.save()
+            return Response(
+                {"message": "Project members removed successfully"},
+                status=status.HTTP_200_OK,
+            )
+
+        except Project.DoesNotExist:
+            return Response(
+                {"error": "Project does not exist"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        return Response(
+            {"error": "invalid method"},
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+        )
+
+    # Add endpoint for assigning project manager
+    @action(
+        detail=True,
+        methods=["POST"],
+        name="Assign Project manager",
+        url_name="assign_project_manager",
+    )
+    def assign_project_manager(self, request, pk=None, *args, **kwargs):
+        if "user_id" in dict(request.data):
+            ids = request.data.get("user_id", "")
+        else:
+            return Response(
+                {"message": "key doesnot match"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        try:
+            project = Project.objects.get(pk=pk)
+            for prj_user in ids:
+                user = User.objects.get(id=prj_user)
+                if user.role == 6:
+                    if project.manager:
+                        return Response(
+                            {"error": "manager already assigned"},
+                            status=status.HTTP_400_BAD_REQUEST,
+                        )
+                        return Response(
+                            {"error": "manager already added"},
+                            status=status.HTTP_400_BAD_REQUEST,
+                        )
+                    else:
+                        project.manager = user
+                        project.save()
+                else:
+                    return Response(
+                        {"error": "user is not manager"},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+            return Response(
+                {"message": "Project manager assigned successfully"},
+                status=status.HTTP_200_OK,
+            )
+        except User.DoesNotExist:
+            return Response(
+                {"error": "User doesnot exist"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        except Project.DoesNotExist:
+            return Response(
+                {"error": "Project does not exist"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        return Response(
+            {"error": "invalid method"},
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+        )
+
+    # add an endpoint to unassign project manager
+    @action(
+        detail=True,
+        methods=["POST"],
+        name="Unassign Project manager",
+        url_name="unassign_project_manager",
+    )
+    def unassign_project_manager(self, request, pk=None, *args, **kwargs):
+        if "user_id" in dict(request.data):
+            ids = request.data.get("user_id", "")
+        else:
+            return Response(
+                {"message": "key doesnot match"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        try:
+            project = Project.objects.get(pk=pk)
+            for prj_user in ids:
+                user = User.objects.get(id=prj_user)
+                if user.role == 6:
+                    if project.manager:
+                        project.manager = None
+                        project.save()
+                    else:
+                        return Response(
+                            {"error": "manager not assigned"},
+                            status=status.HTTP_400_BAD_REQUEST,
+                        )
+                else:
+                    return Response(
+                        {"error": "user is not manager"},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+            return Response(
+                {"message": "Project manager unassigned successfully"},
+                status=status.HTTP_200_OK,
+            )
+        except User.DoesNotExist:
+            return Response(
+                {"error": "User doesnot exist"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        except Project.DoesNotExist:
+            return Response(
+                {"error": "Project does not exist"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        return Response(
+            {"error": "invalid method"},
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+        )
+
+    # Add endpoint for archiving a project
+    @action(
+        detail=True,
+        methods=["POST", "GET"],
+        name="Archive Project",
+        url_name="archive_project",
+    )
+    def archive_project(self, request, pk=None, *args, **kwargs):
+        try:
+            project = Project.objects.get(pk=pk)
+            project.is_archived = not project.is_archived
+            project.save()
+            return Response(
+                {"message": "Project archived successfully"},
+                status=status.HTTP_200_OK,
+            )
+        except Project.DoesNotExist:
+            return Response(
+                {"error": "Project does not exist"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        return Response(
+            {"error": "invalid method"},
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+        )
