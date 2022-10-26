6c954f452c1156e683a012a00907b89a8d39e5ba
gokulnc@ymail.com <gokulnc@ymail.com>
2022-10-26 10:16:35 +0000

    Add WhiteNoise to serve static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
