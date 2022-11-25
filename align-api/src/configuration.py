e7d667743902303c62cdb9e4385c1f114bb1d00b
agupta54 <anirudhgupta12@.com>
2022-11-25 13:06:04 +0530

    api for align using frames

--- align-api/src/configuration.py
+++ align-api/src/configuration.py
@@ -4,8 +4,8 @@ from dataclasses import dataclass
 @dataclass(order=True)
 class ModelPath:
     wav2vec2_path: str = "models/wav2vec2/indic"
-    language_codes = ["en", "hi"]
-    device = 'cpu'
+    language_codes = ["hi"]
+    device = 'cuda' # cuda or cpu
 
 
 @dataclass(order=True)
@@ -13,4 +13,4 @@ class Data:
     wav_path: str = "audio/english.wav"
     txt_path: str = "../examples/sample.txt"
     srt_path: str = "srt/English_corrected.srt"
-    language: str = "en"
+    language: str = "hi"
--- align-api/src/main.py
+++ align-api/src/main.py
@@ -1,15 +1,49 @@
-from configuration import ModelPath
+from configuration import ModelPath, Data
 from wav2vec2.utils import Wav2vec2
 from rich.console import Console
 from rich.traceback import install
+from fastapi import FastAPI
+from pydantic import BaseModel
+from pydub import AudioSegment
+import numpy as np
+import torch
+from utils import filter_text, SubtitleTimestamps
+from typing import Optional
 
 install()
 console = Console()
+
+app = FastAPI()
+
+class AlignData(BaseModel):
+    text: str = None
+    wav_chunk: list = None
+    start_time: Optional[float] = 0.0
+    language: str = None
+
 
 language_codes = ModelPath.language_codes
 aligner_models = {}
 for language in language_codes:
     console.log(f"Loading aligner model for language {language}")
     aligner_models[language] = Wav2vec2(
-        ModelPath.wav2vec2_path, language_code=language, mode="tensor",
-        device=ModelPath.device)
+        ModelPath.wav2vec2_path,
+        language_code=language,
+        mode="tensor",
+        device=ModelPath.device,
+    )
+
+@app.post("/")
+def align(align_data: AlignData):
+    if align_data.language not in language_codes:
+        raise Exception(f"{align_data.language} model is not loaded")
+    alignment = {}
+    float_wav = np.array(align_data.wav_chunk)
+    wav_tensor = torch.from_numpy(float_wav).float().view(1, -1)
+    cleaned_text =  filter_text(align_data.text, align_data.language)
+    word_segments = aligner_models[align_data.language].merge_words(
+        wav_tensor, cleaned_text, begin=align_data.start_time
+    )
+    alignment["text"] = align_data.text
+    alignment["timestamps"] = word_segments
+    return alignment
