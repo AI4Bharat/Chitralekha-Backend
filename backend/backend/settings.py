f28d5a2588b119c2fc833fe1324b28cef071669a
ummegith <ummaimaec.rymec@gmail.com>
2022-11-28 08:51:27 +0530

    adding linters

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -153,9 +153,9 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
-     'DEFAULT_PERMISSION_CLASSES': [
-      'rest_framework.permissions.IsAuthenticated',
-   ]
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
 
 # Database
