520d216670ee1e26ec254ec0de460baa2a015ba7
Shruti1229 <shrgupta01@gmail.com>
2023-01-18 13:02:08 +0530

    Change permissions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -152,10 +152,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
-    ],
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ],
+    ]
 }
 
 # Database
