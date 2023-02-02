e62b4fc192a94b123705bb105e613a00256a2e07
Shruti1229 <shrgupta01@gmail.com>
2023-02-02 18:00:14 +0530

    Deletion with flag

--- backend/video/urls.py
+++ backend/video/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("delete_video", views.delete_video, name="delete_video"),
     path("list_recent", views.list_recent, name="list_recent"),
     path("list_tasks", views.list_tasks, name="list_tasks"),
+    path("download_all", views.download_all, name="download_all"),
 ]
