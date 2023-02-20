61a571a0adc56263b0212efa810a8936f7081604
dheeraj gujral <dheerajgujral47@gmail.com>
2023-02-20 20:44:06 +0530

    Making asr api call asynchronous with Celery and Redis

---
+++ backend/backend/celery.py
@@ -0,0 +1,12 @@
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+app = Celery(
+    "backend",
+    accept_content=["application/json"],
+    result_serializer="json",
+    task_serializer="json",
+)
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
