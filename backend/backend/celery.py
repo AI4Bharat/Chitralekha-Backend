c2a8539aad9d7d9bf1946517bb1380f52c041cf0
Shruti1229 <shrgupta01@gmail.com>
2023-04-13 12:13:02 +0530

    Schedule tasks

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -1,5 +1,7 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 app = Celery(
@@ -14,4 +16,12 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 # Celery Queue related settings
 app.conf.task_default_queue = "default"
 app.conf.task_routes = {"task.tasks.*": {"queue": "task"}}
+
+app.conf.beat_schedule = {
+    "Send_mail_to_Clients": {
+        "task": "send_mail_task",
+        "schedule": crontab(minute=0, hour="*/2"),  # execute 4 times in a day
+    }
+}
+
 app.autodiscover_tasks()
