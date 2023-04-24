bf9c53ae8ce6c44bd0104dafea24841d14a9c6b9
gokulnc@ymail.com <gokulnc@ymail.com>
2023-04-24 08:35:57 +0000

    Deployment changes

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 # FIXME: Temporarily, eng-ASR is hosted separately. Will be merged soon into `asr_url`
 english_asr_url = "http://216.48.183.5:7001/transcribe"
+english_asr_url = os.getenv("ASR_API_URL")
 asr_url = os.getenv("ASR_API_URL")
 tts_url = os.getenv("TTS_API_URL")
 anuvaad_url = os.getenv("ANUVAAD_NMT_URL")
