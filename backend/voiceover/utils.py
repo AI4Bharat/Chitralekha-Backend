e3b06a70e186a642fa44675693cd114499c2a4c3
Chetan-Gudagamanal <chetanbg95@gmail.com>
2023-11-22 13:22:55 +0530

    added check for empty cards while translation complete

--- backend/voiceover/utils.py
+++ backend/voiceover/utils.py
@@ -523,6 +523,41 @@ def get_bad_sentences_in_progress(translation_obj, target_language):
                 pass
     return problem_sentences
 
+def get_empty_cards(obj, task, isTranscription):
+    empty_cards=[]
+    data = obj.payload
+    for ind, textCard in enumerate(data["payload"]):
+        if isTranscription==False:
+            if (
+                ("text" in textCard.keys() and len(textCard['text'])<1)
+                or ("target_text" in textCard.keys() and len(textCard['target_text'])<1)
+            ):
+                empty_cards.append(
+                    {
+                        "page_number": (ind // 50) + 1,
+                        "index": (ind % 50) + 1,
+                        "start_time": textCard["start_time"],
+                        "end_time": textCard["end_time"],
+                        "text": textCard["text"],
+                        "target_text": textCard["target_text"],
+                    }
+                )
+        else:
+            if (
+                "text" in textCard.keys()
+                and len(textCard['text'])<1
+            ):
+                empty_cards.append(
+                    {
+                        "page_number": (ind // 50) + 1,
+                        "index": (ind % 50) + 1,
+                        "start_time": textCard["start_time"],
+                        "end_time": textCard["end_time"],
+                        "text": textCard["text"],
+                    }
+                )
+    return empty_cards
+
 
 def process_translation_payload(translation_obj, target_language):
     tts_input = []
