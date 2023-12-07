cb53f55eccfeef193b8e6a29a811f44b2f5823eb
Shruti1229 <shrgupta01@gmail.com>
2023-12-07 15:46:15 +0530

    Subscribing and Unsubscribing changes

--- backend/newsletter/serializers.py
+++ backend/newsletter/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
-from .models import Newsletter
+from .models import Newsletter, SubscribedUsers
 from django.db import migrations, models
 
 
 class NewsletterSerializer(serializers.ModelSerializer):
     content = serializers.ListField(child=serializers.DictField())
+
     class Meta:
         model = Newsletter
         fields = ("newsletter_uuid", "submitter_id", "content", "category")
+
+class SubscribedUsersSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = SubscribedUsers
+        fields = ("user")
