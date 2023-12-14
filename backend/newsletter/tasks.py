58729aabb7cc42da14736fd5ffccf7a631438cd3
Shruti1229 <shrgupta01@gmail.com>
2023-12-14 22:01:31 +0530

    New worker in newsletter

--- backend/newsletter/tasks.py
+++ backend/newsletter/tasks.py
@@ -1,37 +1,27 @@
-from celery import shared_task
 from backend.celery import celery_app
 from .models import SubscribedUsers, Newsletter
-from django.utils import timezone
 from django.conf import settings
 from django.core.mail import send_mail
 import logging
 
 
-@shared_task(name="send_newsletter")
-def send_newsletter():
+@celery_app.task(queue="newsletter")
+def celery_newsletter_call(newsletter_id):
     logging.info("Sending Newsletter")
-    now = timezone.now()
-    past_24_hours = now - timezone.timedelta(hours=24)
-
-    # Get all objects created in the past 24 hours
-    newsletters = Newsletter.objects.filter(created_at__gte=past_24_hours).all()
-    if newsletters is not None:
-        subscribed_users = SubscribedUsers.objects.all()
-        for newsletter in newsletters:
-            for subscribed_user in subscribed_users:
-                logging.info("Sending Mail to %s", subscribed_user.user.email)
-                cont = newsletter.content.replace(
-                    "{user_email_id}", subscribed_user.email
-                )
-                newsletter.content = cont
-                newsletter.save()
-                try:
-                    send_mail(
-                        "Chitralekha - Newsletter",
-                        "",
-                        settings.DEFAULT_FROM_EMAIL,
-                        [subscribed_user.email],
-                        html_message=newsletter.content,
-                    )
-                except:
-                    logging.info("Mail can't be sent.")
+    subscribed_users = SubscribedUsers.objects.all()
+    newsletter = Newsletter.objects.get(pk=newsletter_id)
+    for subscribed_user in subscribed_users:
+        logging.info("Sending Mail to %s", subscribed_user.user.email)
+        cont = newsletter.content.replace("{user_email_id}", subscribed_user.email)
+        newsletter.content = cont
+        newsletter.save()
+        try:
+            send_mail(
+                "Chitralekha - Newsletter",
+                "",
+                settings.DEFAULT_FROM_EMAIL,
+                [subscribed_user.email],
+                html_message=newsletter.content,
+            )
+        except:
+            logging.info("Mail can't be sent.")
