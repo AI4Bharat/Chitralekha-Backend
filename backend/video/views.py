0143b807aa69d504cfbbab8f0e4cd80622011738
kartikvirendrar <kartikrajput357@gmail.com>
2025-06-23 10:51:05 +0530

    add asr support for english language

--- backend/video/views.py
+++ backend/video/views.py
@@ -122,7 +122,7 @@ class TranscribeAPIView(APIView):
 
         chunk_data = {
             "config": {
-                "serviceId": os.getenv("DHRUVA_SERVICE_ID"),
+                "serviceId": os.getenv("DHRUVA_SERVICE_ID") if lang != "en" else os.getenv("DHRUVA_SERVICE_ID_EN"),
                 "language": {"sourceLanguage": lang},
                 "transcriptionFormat": {"value": "transcript"}
                 },
